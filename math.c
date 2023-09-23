5b1494419e9749602214ca54eee36b3f0dc8169f
Jan0310 <kjannani20@gmail.com>
2023-09-23 14:00:52 +0530

    math file created

---
+++ math.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int x = 2;
+    int y = 12;
+    int power = pow (x , y);
+    printf("%d power of %d is : %d",y ,x ,power);
+    return 0;
+}
